68bbdcd972c92072eb7814804342a94f5b555e45
ada.kreci <ada.kreci@iodigital.com>
2023-04-10 13:28:04 +0200

    refactoring, using java streams

--- src/main/java/com/abnamro/recipevault/service/RecipeSpecificationQueryService.java
+++ src/main/java/com/abnamro/recipevault/service/RecipeSpecificationQueryService.java
@@ -1,56 +1,69 @@
 package com.abnamro.recipevault.service;
 
 import com.abnamro.recipevault.domain.Recipe;
-import com.abnamro.recipevault.domain.RecipeIngredient;
 import com.abnamro.recipevault.request.RecipeRequest;
-import jakarta.persistence.criteria.*;
-import org.apache.commons.collections4.CollectionUtils;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 @Service
 public class RecipeSpecificationQueryService {
 
-    public Specification<Recipe> getSearchQuery( final RecipeRequest request) {
-        return (root, query, criteriaBuilder) -> {
+    public Specification<Recipe> getSearchQuery( final RecipeRequest request ) {
+        return ( root, query, criteriaBuilder ) -> {
             List<Predicate> predicates = new ArrayList<>();
 
-            handleSameFields(request, root, criteriaBuilder, predicates);
-            if (CollectionUtils.isNotEmpty(request.getExcludeIngredients())
-                    || CollectionUtils.isNotEmpty(request.getIncludeIngredients())) {
-                Join<Recipe, RecipeIngredient> join = root.join("ingredients");
-                Path<String> ingredientName = join.get("ingredient").get("name");
+            handleSameFields( request, root, criteriaBuilder, predicates );
+            handleIngredients( request, root, criteriaBuilder, predicates );
 
-                if (CollectionUtils.isNotEmpty(request.getExcludeIngredients())) {
-                    predicates.add(ingredientName.in(request.getExcludeIngredients()).not());
-                }
-                if (CollectionUtils.isNotEmpty(request.getIncludeIngredients())) {
-                    predicates.add(ingredientName.in(request.getIncludeIngredients()));
-                }
-            }
-            return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
+            return criteriaBuilder.and( predicates.toArray( new Predicate[0] ) );
         };
     }
 
-    private void handleSameFields(final RecipeRequest request,
-                                  Root<Recipe> root,
-                                  CriteriaBuilder criteriaBuilder,
-                                  List<Predicate> predicates) {
-        if (Boolean.TRUE.equals(request.getVegetarian())) {
-            predicates.add(criteriaBuilder.isTrue(root.get("vegetarian")));
+    private void handleSameFields( final RecipeRequest request,
+                                   Root<Recipe> root,
+                                   CriteriaBuilder criteriaBuilder,
+                                   List<Predicate> predicates ) {
+        Optional.ofNullable( request.getVegetarian() )
+                .ifPresent( vegetarian -> predicates.add( criteriaBuilder.equal( root.get( "vegetarian" ), vegetarian ) ) );
+        Optional.ofNullable( request.getServings() )
+                .ifPresent( servings -> predicates.add( criteriaBuilder.equal( root.get( "servings" ), servings ) ) );
+        Optional.ofNullable( request.getSearchKey() )
+                .filter( StringUtils::isNotBlank )
+                .ifPresent( searchKey -> predicates.add( criteriaBuilder.like( root.get( "instructions" ), "%" + searchKey + "%" ) ) );
+    }
+
+    private void handleIngredients( final RecipeRequest request,
+                                    Root<Recipe> root,
+                                    CriteriaBuilder criteriaBuilder,
+                                    List<Predicate> predicates ) {
+        Predicate ingredientPredicate = Optional.ofNullable( request.getExcludeIngredients() )
+                                                .map( Collection::stream )
+                                                .orElse( Stream.empty() )
+                                                .map( excludeIngredient -> criteriaBuilder.not( root.join( "ingredients" ).get( "ingredient" ).get( "name" ).in( excludeIngredient ) ) )
+                                                .reduce( criteriaBuilder::and )
+                                                .orElse( null );
+        if ( ingredientPredicate != null ) {
+            predicates.add( ingredientPredicate );
         }
-        if (Boolean.FALSE.equals(request.getVegetarian())) {
-            predicates.add(criteriaBuilder.isFalse(root.get("vegetarian")));
-        }
-        if (request.getServings() != null) {
-            predicates.add(criteriaBuilder.equal(root.get("servings"), request.getServings()));
-        }
-        if (StringUtils.isNotBlank(request.getSearchKey())) {
-            predicates.add(criteriaBuilder.like(root.get("instructions"), "%" + request.getSearchKey() + "%"));
+
+        ingredientPredicate = Optional.ofNullable( request.getIncludeIngredients() )
+                                      .map( Collection::stream )
+                                      .orElse( Stream.empty() )
+                                      .map( includeIngredient -> root.join( "ingredients" ).get( "ingredient" ).get( "name" ).in( includeIngredient ) )
+                                      .reduce( criteriaBuilder::or )
+                                      .orElse( null );
+        if ( ingredientPredicate != null ) {
+            predicates.add( ingredientPredicate );
         }
     }
 }
--- src/main/java/com/abnamro/recipevault/service/RecipeValidationService.java
+++ src/main/java/com/abnamro/recipevault/service/RecipeValidationService.java
@@ -24,38 +24,43 @@ public class RecipeValidationService {
     private final WebMessageResolverService webMessageResolverService;
 
     public void validateRecipeCreate(final RecipeDTO recipeRequest) throws RecipeValidationException {
-        final boolean recipeExists = recipeRepository.existsByNameIgnoreCase(recipeRequest.getName());
-        if (recipeExists) {
-            throw new RecipeValidationException(webMessageResolverService.getMessage("Exists.recipe.name", recipeRequest.getName()));
+        final boolean recipeExists = recipeRepository.existsByNameIgnoreCase( recipeRequest.getName() );
+        if ( recipeExists ) {
+            throw new RecipeValidationException( webMessageResolverService.getMessage( "Exists.recipe.name", recipeRequest.getName() ) );
         }
 
-        for (final IngredientDTO ingredient : recipeRequest.getIngredients()) {
-            final boolean ingredientExists = ingredientRepository.existsByNameIgnoreCase(ingredient.getName());
-            if (ingredientExists) {
-                throw new RecipeValidationException(webMessageResolverService.getMessage("Exists.ingredient.name", ingredient.getName()));
-            }
-        }
+        recipeRequest.getIngredients().stream()
+                     .map( IngredientDTO::getName )
+                     .map( String::toLowerCase )
+                     .filter( ingredientName -> ingredientRepository.existsByNameIgnoreCase( ingredientName ) )
+                     .findFirst()
+                     .ifPresent( ingredientName -> {
+                         throw new RecipeValidationException( webMessageResolverService.getMessage( "Exists.ingredient.name", ingredientName ) );
+                     } );
     }
 
     public void validateRecipeUpdate(final Recipe recipe, final RecipeDTO recipeRequest) throws RecipeValidationException {
-        if (ObjectUtils.notEqual(recipe.getName(), recipeRequest.getName())) {
-            final boolean recipeExists = recipeRepository.existsByNameIgnoreCase(recipeRequest.getName());
-            if (recipeExists) {
-                throw new RecipeValidationException(webMessageResolverService.getMessage("Exists.recipe.name", recipeRequest.getName()));
+        if ( ObjectUtils.notEqual( recipe.getName(), recipeRequest.getName() ) ) {
+            final boolean recipeExists = recipeRepository.existsByNameIgnoreCase( recipeRequest.getName() );
+            if ( recipeExists ) {
+                throw new RecipeValidationException( webMessageResolverService.getMessage( "Exists.recipe.name", recipeRequest.getName() ) );
             }
         }
 
         final Map<Long, RecipeIngredient> mapByIngredientId = recipe.getIngredients().stream()
-                                                                    .collect(Collectors.toMap(recipeIngredient -> recipeIngredient.getIngredient().getIngredientId(), Function.identity()));
-        for (final IngredientDTO ingredient : recipeRequest.getIngredients()) {
-            final RecipeIngredient foundMatch = mapByIngredientId.get(ingredient.getIngredientId());
-            if (foundMatch == null || (foundMatch.getIngredient() != null && ObjectUtils.notEqual(ingredient.getName(), foundMatch.getIngredient().getName()))) {
-                final boolean ingredientExists = ingredientRepository.existsByNameIgnoreCase(ingredient.getName());
-                if (ingredientExists) {
-                    throw new RecipeValidationException(webMessageResolverService.getMessage("Exists.ingredient.name", ingredient.getName()));
-                }
-            }
-        }
-    }
+                                                                    .collect( Collectors.toMap( recipeIngredient -> recipeIngredient.getIngredient().getIngredientId(), Function.identity() ) );
 
+        recipeRequest.getIngredients().stream()
+                     .filter( ingredient -> {
+                         RecipeIngredient foundMatch = mapByIngredientId.get( ingredient.getIngredientId() );
+                         return foundMatch == null || (foundMatch.getIngredient() != null && ObjectUtils.notEqual( ingredient.getName(), foundMatch.getIngredient().getName() ));
+                     } )
+                     .map( IngredientDTO::getName )
+                     .map( String::toLowerCase )
+                     .filter( ingredientName -> ingredientRepository.existsByNameIgnoreCase( ingredientName ) )
+                     .findFirst()
+                     .ifPresent( ingredientName -> {
+                         throw new RecipeValidationException( webMessageResolverService.getMessage( "Exists.ingredient.name", ingredientName ) );
+                     } );
+    }
 }
